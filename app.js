b3e2bc3983922c34a630e7cadf4a6b4c795d8be5
Louis Fernando Medina <louisfm16@gmail.com>
2019-12-01 01:50:57 -0800

    idk what I did here

--- app.js
+++ app.js
@@ -7,19 +7,25 @@ document.addEventListener("DOMContentLoaded", function(){
     gScene.addEventListener("mouseleave", function() {
         lScene.classList.remove("scene-40");
         lScene.classList.remove("scene-60");
+        lScene.children[0].classList.remove("scene_zoom-out");
         rScene.classList.remove("scene-40");
         rScene.classList.remove("scene-60");
+        rScene.children[0].classList.remove("scene_zoom-out");
     });
 
     lScene.addEventListener("mouseenter", function() {
         rScene.classList.remove("scene-60");
+        rScene.children[0].classList.remove("scene_zoom-out");
         this.classList.add("scene-60");
+        this.children[0].classList.add("scene_zoom-out");
         rScene.classList.add("scene-40");
     });
 
     rScene.addEventListener("mouseenter", function() {
         lScene.classList.remove("scene-60");
+        lScene.children[0].classList.remove("scene_zoom-out");
         this.classList.add("scene-60");
+        this.children[0].classList.add("scene_zoom-out");
         lScene.classList.add("scene-40");
     });
 });
